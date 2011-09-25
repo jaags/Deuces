9e05ee867261d5a976c4f904ffc433a66bcf20f0
Shashwat Kandadai <shashwatak@gmail.com>
2011-09-25 02:09:19 -0700

    Updtaed README, added two files

---
+++ Deuces_client.py
@@ -0,0 +1,7 @@
+"""
+The client, displays the UI based on the game's state,
+accepts and sends user commands to the server.
+Text based UI
+"""
+
+#NOTHING HERE, PROBLEM?
---
+++ Deuces_serv.py
@@ -0,0 +1,7 @@
+"""
+The server code, will contain all the game logic,
+receiving the player's commands from the client and
+serving the client the game's state as it changes
+"""
+
+#LOL EMPTY
